7627ed13e667421322a272dde2cbb1fb438af3c7
Okan Bakırcı <132247985+bkrcokan13@users.noreply.github.com>
2024-01-14 22:05:38 +0300

    Update style.css

--- js/script.js
+++ js/script.js
@@ -9,18 +9,18 @@ function getCars() {
         {
             category: "SEDANS",
             description: "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
-            carIcon: "images/icon-sedans.svg"
+            carIcon: "img/icon-sedans.svg"
         },
         {
             category: "SUVS",
             description: "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
-            carIcon : "images/icon-suvs.svg",
+            carIcon : "img/icon-suvs.svg",
         
         },
         {
             category: "LUXURY",
             description: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
-            carIcon: "images/icon-luxury.svg"
+            carIcon: "img/icon-luxury.svg"
         }
     ];
 
